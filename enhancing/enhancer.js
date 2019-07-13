da198e39ab6fce9e0c0cc01d656c4b7cb5dcced7
Antony Nyagah <anttonynyagah@Antonys-MBP-2.attlocal.net>
2019-07-13 14:12:55 -0400

    initial commit

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,43 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+
+  const result = { ...item }
+
+  result.enhancement !== 20 ? result.enhancement = item.enhancement + 1
+                            : null; 
+
+  return result;
 }
 
 function fail(item) {
-  return { ...item };
+  
+  const result = { ...item }
+
+  if(result.enhancement < 15) {
+    result.durability = item.durability - 5
+  } else if(result.enhancement < 17) {
+    result.durability = item.durability - 10
+  } else {
+    result.durability = item.durability - 1
+  }
+
+  return result;
 }
 
 function repair(item) {
-  return { ...item };
+  const result = { ...item }
+
+  result.durability = 100;
+
+  return result;
 }
 
 function get(item) {
-  return { ...item };
+  const result = { ...item }
+
+  result.enhancement !== 0 ? result.name = `[+${result.enhancement}] ${result.name}`
+                           : null;
+
+  return result;
 }
